a81d1cb12031606443a6b416ab38a084841da645
Ondine Taukolo <ondine.t05@gmail.com>
2026-02-27 13:31:47 +1100

    Premier commit - projet Symfony avengers

---
+++ src/Controller/MarquepageController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Marquepage;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MarquepageController extends AbstractController
+{
+    #[Route('/marquepages', name: 'app_marquepages')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $marquepages = $entityManager
+            ->getRepository(Marquepage::class)
+            ->findAll();
+
+        return $this->render('marquepage/index.html.twig', [
+            'marquepages' => $marquepages,
+        ]);
+    }
+
+    #[Route('/marquepages/ajouter', name: 'app_marquepages_ajouter')]
+    public function ajouter(EntityManagerInterface $entityManager): Response
+    {
+        $mp1 = new Marquepage();
+        $mp1->setUrl('https://symfony.com');
+        $mp1->setDateCreation(new \DateTime());
+        $mp1->setCommentaire('Le site officiel de Symfony');
+
+        $mp2 = new Marquepage();
+        $mp2->setUrl('https://www.qwant.com');
+        $mp2->setDateCreation(new \DateTime());
+        $mp2->setCommentaire('Moteur de recherche Qwant');
+
+        $mp3 = new Marquepage();
+        $mp3->setUrl('https://www.github.com');
+        $mp3->setDateCreation(new \DateTime());
+        $mp3->setCommentaire('GitHub');
+
+        $entityManager->persist($mp1);
+        $entityManager->persist($mp2);
+        $entityManager->persist($mp3);
+
+        $entityManager->flush();
+
+        return new Response('Les 3 marque-pages ont été sauvegardés en base !');
+    }
+}
---
+++ src/Entity/Marquepage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MarquepageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MarquepageRepository::class)]
+class Marquepage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $commentaire = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateCreation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): static
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTime
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTime $dateCreation): static
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+}
